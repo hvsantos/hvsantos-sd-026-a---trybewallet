81c99e22006812870ed00d46b5187d378e697a4d
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 12:47:44 -0300

    create savveUser action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+const SAVE_USER_EMAIL = 'SAVE_USER_EMAIL';
+
+const saveUser = (email) => ({
+  type: SAVE_USER_EMAIL,
+  payload: email,
+});
+
+export default saveUser;
