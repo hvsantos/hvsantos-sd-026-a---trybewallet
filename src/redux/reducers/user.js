bcefc68d66bc0b01732bbb7fa2d8c4c11c9b8a8f
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 12:48:03 -0300

    create user Reducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const USER_INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = USER_INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'SAVE_USER_EMAIL':
+    return { ...state,
+      email: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default user;
