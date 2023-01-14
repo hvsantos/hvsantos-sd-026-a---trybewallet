79cc75440d7661b7aab6c306d9782fa76a72a6b7
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 16:16:37 -0300

    change actions to support fetch

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,20 @@
+import fetchPqSim from '../../helper/fetchPqSim';
+
 // Coloque aqui suas actions
 const SAVE_USER_EMAIL = 'SAVE_USER_EMAIL';
+const SAVE_WALLET_CURRENCY = 'SAVE_WALLET_CURRENCY';
 
-const saveUser = (email) => ({
+export const saveUser = (email) => ({
   type: SAVE_USER_EMAIL,
   payload: email,
 });
 
-export default saveUser;
+export const saveCurrency = (func) => {
+  fetchPqSim().then((arr) => {
+    const dataArr = arr.map((elem) => elem.id);
+    func({
+      type: SAVE_WALLET_CURRENCY,
+      payload: dataArr,
+    });
+  });
+};
