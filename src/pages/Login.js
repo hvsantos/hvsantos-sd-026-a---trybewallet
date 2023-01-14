dccea0175652cde6684c4ee0c343c613df9d86e4
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 12:50:14 -0300

    create Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import saveUser from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    inputEmail: '',
+    inputPassword: '',
+    redirectToWallet: false,
+    disableButton: true,
+  };
+
+  submitAndRedirect = (event) => {
+    event.preventDefault();
+
+    const { dispatch } = this.props;
+    const { inputEmail } = this.state;
+
+    dispatch(saveUser(inputEmail));
+
+    this.setState({ redirectToWallet: true });
+  };
+
+  handleChanger = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => {
+      const { inputEmail, inputPassword } = this.state;
+      const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+      const testRegex = regex.test(inputEmail);
+      const seis = 6;
+
+      this.setState({ disableButton: !(testRegex && inputPassword.length >= seis) });
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { redirectToWallet, disableButton, inputEmail, inputPassword } = this.state;
+
+    if (redirectToWallet) {
+      return <Redirect to="/carteira" />;
+    }
+
+    return (
+      <div>
+        <label htmlFor="inputEmail">
+          Email
+          <input
+            name="inputEmail"
+            data-testid="email-input"
+            value={ inputEmail }
+            onChange={ this.handleChanger }
+          />
+        </label>
+
+        <label htmlFor="inputPassword">
+          Senha
+          <input
+            name="inputPassword"
+            data-testid="password-input"
+            value={ inputPassword }
+            onChange={ this.handleChanger }
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={ this.submitAndRedirect }
+          disabled={ disableButton }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+export default connect()(Login);
+
+Login.propTypes = {}.isRequired;
