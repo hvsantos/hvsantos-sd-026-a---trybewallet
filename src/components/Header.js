fffe01d8bbb16ac6d34b53266203bd4395da7f00
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 13:07:08 -0300

    create Header for Wallet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    console.log(this.props);
+    const {
+      user: { email },
+      wallet: { expenses },
+    } = this.props;
+
     return (
-      <div>Header</div>
+      <div>
+        <h1>TRYBE WALLET</h1>
+        <div>
+          <h3>
+            Email:
+            {' '}
+            <span data-testid="email-field">
+              { email }
+            </span>
+          </h3>
+          <h3>
+            Despesa Total:
+            {' '}
+            <span data-testid="total-field">
+              { expenses.length !== 0
+                ? expenses.reduce((a, b) => a + b, 0)
+                : 0 }
+            </span>
+            {' '}
+            <span data-testid="header-currency-field">
+              BRL
+            </span>
+          </h3>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ user, wallet }) => ({ user, wallet });
+
+Header.propTypes = {}.isRequired;
+
+export default connect(mapStateToProps)(Header);
