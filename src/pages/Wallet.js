3077841e581d9b706c259b0b6440ef83a7eff2d9
Henrique Vilela <hv_santos@outlook.com>
2023-01-14 13:07:35 -0300

    fix Wallet - removed unused lines

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    console.log(this.props);
-    const { dispatch,
-      state: {
-        wallet: { currencies, editor, expenses, idToEdit },
-      } } = this.props;
-
     return (
       <div>
         <Header />
@@ -20,8 +13,4 @@ class Wallet extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({ state });
-
-Wallet.propTypes = {}.isRequired;
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
