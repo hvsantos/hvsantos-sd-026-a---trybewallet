99c078c3f373fdb3ff7b4727071decf98d851943
Henrique Vilela <hv_santos@outlook.com>
2023-01-15 13:42:57 -0300

    refactor fetch function - to fix req 4

--- src/helper/fetchPqSim.js
+++ src/helper/fetchPqSim.js
@@ -1,12 +1,7 @@
 const fetchPqSim = () => {
   const url = 'https://economia.awesomeapi.com.br/json/all';
   const dataArr = fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const arrOfObj = Object.entries(data)
-        .map(([key, value]) => ({ ...value, id: key }));
-      return arrOfObj.filter((curren) => curren.id !== 'USDT');
-    });
+    .then((response) => response.json());
   return dataArr;
 };
 
