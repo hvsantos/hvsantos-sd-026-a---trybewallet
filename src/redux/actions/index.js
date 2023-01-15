db5a06c45a0b74e1df6e5cd4464deb866f0f95e9
Henrique Vilela <hv_santos@outlook.com>
2023-01-15 13:44:19 -0300

    feat action saveExpenses - fix saveCurrency

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,10 @@
 import fetchPqSim from '../../helper/fetchPqSim';
+import setIds from '../../helper/setIds';
 
 // Coloque aqui suas actions
 const SAVE_USER_EMAIL = 'SAVE_USER_EMAIL';
 const SAVE_WALLET_CURRENCY = 'SAVE_WALLET_CURRENCY';
+const SAVE_WALLET_EXPENSES = 'SAVE_WALLET_EXPENSES';
 
 export const saveUser = (email) => ({
   type: SAVE_USER_EMAIL,
@@ -10,11 +12,40 @@ export const saveUser = (email) => ({
 });
 
 export const saveCurrency = (func) => {
-  fetchPqSim().then((arr) => {
-    const dataArr = arr.map((elem) => elem.id);
+  fetchPqSim().then((data) => {
+    const arrOfObj = Object.entries(data)
+      .map(([key, value]) => ({ ...value, id: key }))
+      .filter((curren) => curren.id !== 'USDT');
+    const dataArr = arrOfObj.map((elem) => elem.id);
     func({
       type: SAVE_WALLET_CURRENCY,
       payload: dataArr,
     });
   });
+};
+
+export const saveExpenses = (func, incState, expenses) => {
+  const { expenseValue, expenseCurrency, expensePayMethod,
+    expenseCategory, expenseDescription } = incState;
+
+  fetchPqSim()
+    .then((fetchedValues) => {
+      const formatExpense = {
+        id: '',
+        value: expenseValue,
+        currency: expenseCurrency,
+        method: expensePayMethod,
+        tag: expenseCategory,
+        description: expenseDescription,
+        exchangeRates: fetchedValues,
+      };
+      return formatExpense;
+    })
+    .then((newExpense) => {
+      expenses.push(newExpense);
+      func({
+        type: SAVE_WALLET_EXPENSES,
+        payload: setIds(expenses),
+      });
+    });
 };
